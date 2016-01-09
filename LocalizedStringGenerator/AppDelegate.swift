ee05a3eb8c29fcc30a56cd4d9930973335e4f573
Stan Wu <stan.wu@me.com>
2016-01-09 19:27:18 +0800

    Initial Commit

---
+++ LocalizedStringGenerator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LocalizedStringGenerator
+//
+//  Created by Stan Wu on 16/1/9.
+//  Copyright © 2016年 Stan Wu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
